8ee80ce58fd7ed5698f627dee389d7f78ea76b90
Andrew-Sem <avvyayvayva@gmail.com>
2023-02-03 11:08:13 +0300

    Reverse words

---
+++ Reverse words/index.ts
@@ -0,0 +1,9 @@
+// Complete the function that accepts a string parameter,
+// and reverses each word in the string. All spaces in the string should be retained.
+
+export function reverseWords(str: string): string {
+  return str
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""))
+    .join(" ");
+}
